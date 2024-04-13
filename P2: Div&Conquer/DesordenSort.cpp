c736c000d3824938d2114a1a939fb50be43d7564
Dani Bustos <danielbustoslangmandel@gmail.com>
2024-04-13 15:57:23 -0300

    comienzo desorden

---
+++ P2: Div&Conquer/DesordenSort.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define fore(i,a,b) for(int i = a; i<b;i++)
+
+using namespace std;
+const long long INDEF = -1;
+vector<int> a;
+//idea
+ 
+
+bool solve(int posInicial, int posFinal){
+
+}
+
+int main(){
+    int n;
+    cin >> n;
+    a.resize(n); fore(i,0,n) cin >> a[i];
+    cout << solve(0,n);
+}
+
