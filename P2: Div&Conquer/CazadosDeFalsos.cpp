bcb3901555301b28c9965ae20ab4bfb5d8729657
Dani-Bustos <110747985+Dani-Bustos@users.noreply.github.com>
2024-04-16 17:00:33 -0300

    Update CazadosDeFalsos.cpp

--- P2: Div&Conquer/CazadosDeFalsos.cpp
+++ P2: Div&Conquer/CazadosDeFalsos.cpp
@@ -6,7 +6,7 @@ typedef long long ll;
 
 const int INDEF = -1;
 vector<vector<bool>> mat;
-
+//recurrencia dada por T(N) = T(N/4) + Θ(1) 
 bool conjuncionSubmatriz(int i0,int i1,int j0,int j1){
     bool res = true;
     fore(i,i0,i1){
@@ -23,7 +23,19 @@ pair<int,int> solve(int i0,int i1,int j0,int j1){
         return mat[i0][j0] == true ? {i0,j0} : {-1,-1};
 
     } else{
-        if(conjuncionSubmatriz()) 
+        int ideltaMitad =  (i1-i0)/2;
+        int jdeltaMitad = (j1-j0)/2;
+        if(conjuncionSubmatriz(i0,i0 + ideltaMitad),j0,j0 + jdeltaMitad){
+           return solve(i0,i0+ideltaMitad,j0,j0 + jdeltaMitad);
+        }else if(conjuncionSubmatriz(i0 + ideltaMitad,i1,j0,j0 + jdeltaMitad){
+            return solve(i0 + ideltaMitad,i1,j0,j0 + jdeltaMitad);
+        }else if(conjuncionSubmatriz(i0,i0 + ideltaMitad,j0 + jdeltaMitad,j1){
+            return solve(i0,i0 + ideltaMitad,j0 + jdeltaMitad,j1);
+        }else if( conjuncionSubmatriz(i0 + ideltaMitad,i1,j0 + jdeltaMitad,j1){
+            return solve(i0 + ideltaMitad,i1,j0 + jdeltaMitad,j1);
+            
+        }
+            
     }
 }
 
