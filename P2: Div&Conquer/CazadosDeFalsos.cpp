4ea4e496b907d8ae65cc6d804cf187d3b1030463
Dani Bustos <danielbustoslangmandel@gmail.com>
2024-04-16 23:17:21 -0300

    fin de cazados de falsos, comienzo mergeSelectivo

--- P2: Div&Conquer/CazadosDeFalsos.cpp
+++ P2: Div&Conquer/CazadosDeFalsos.cpp
@@ -6,11 +6,13 @@ typedef long long ll;
 
 const int INDEF = -1;
 vector<vector<bool>> mat;
-//recurrencia dada por T(N) = T(N/4) + Θ(1) 
+//recurrencia dada por T(N) = T(N/4) + Θ(1) -> (teo. maestro) Θ(log(n))
+//Idea: partir la matriz en 4, fijarse en "O(1)" con la funcion conjunciones SUbmatriz para que cuarto hay un false
+//explorar recursivamente en esa direccion
 bool conjuncionSubmatriz(int i0,int i1,int j0,int j1){
     bool res = true;
-    fore(i,i0,i1){
-        fore(j,j0,j1){
+    for(int i = i0 ; i < (i0 == i1 ?  i0 + 1: i1); i++){
+        for(int j = j0; j <(j0 == j1 ?  j0 + 1:  j1) ;j++){
             res = res && mat[i][j];
             if(res == false) return res;
         }
@@ -19,22 +21,36 @@ bool conjuncionSubmatriz(int i0,int i1,int j0,int j1){
 }
 
 pair<int,int> solve(int i0,int i1,int j0,int j1){
-    if(i0 + 1 == i1 && j0 + 1 == j1){
-        return mat[i0][j0] == true ? {i0,j0} : {-1,-1};
+    //estamos en una matriz de 1x1, la 2da condicion hace falta por casos borde
+    if((i0 + 1 == i1 && j0 + 1 == j1) || (i0 == i1 && j0 == j1)){
+        //si la matriz es de 1x1, quiero ver que el falso este
+        if (mat[i0][j0] == false) return {i0,j0}; 
+        else return {-1,-1};
 
-    } else{
+    }else{
         int ideltaMitad =  (i1-i0)/2;
         int jdeltaMitad = (j1-j0)/2;
-        if(conjuncionSubmatriz(i0,i0 + ideltaMitad),j0,j0 + jdeltaMitad){
-           return solve(i0,i0+ideltaMitad,j0,j0 + jdeltaMitad);
-        }else if(conjuncionSubmatriz(i0 + ideltaMitad,i1,j0,j0 + jdeltaMitad){
-            return solve(i0 + ideltaMitad,i1,j0,j0 + jdeltaMitad);
-        }else if(conjuncionSubmatriz(i0,i0 + ideltaMitad,j0 + jdeltaMitad,j1){
-            return solve(i0,i0 + ideltaMitad,j0 + jdeltaMitad,j1);
-        }else if( conjuncionSubmatriz(i0 + ideltaMitad,i1,j0 + jdeltaMitad,j1){
-            return solve(i0 + ideltaMitad,i1,j0 + jdeltaMitad,j1);
+        int iHalf = i0 + ideltaMitad;
+        int jHalf = j0 + jdeltaMitad;
+        //Arriba a la izquierda
+        if(not conjuncionSubmatriz(i0,iHalf,j0,jHalf)){
+           return solve(i0,i0+ideltaMitad,j0,jHalf);
+        
+        }//Abajo a la izquierda
+        else if(not conjuncionSubmatriz(iHalf,i1,j0,jHalf)){
+            return solve(i0 + ideltaMitad,i1,j0,jHalf);
+        
+        }//Arriba derecha
+        else if( not conjuncionSubmatriz(i0,iHalf,jHalf,j1)){
+            return solve(i0,iHalf,jHalf,j1);
+        
+        }//Abajo derecha
+        else if( not conjuncionSubmatriz(iHalf,i1,jHalf,j1)){
+            return solve(iHalf,i1,jHalf,j1);
             
         }
+        //no habia falsos
+        return {-1,-1};
             
     }
 }
@@ -52,7 +68,8 @@ int main(){
             }
 
     }
-    
+   pair<int,int> res = solve(0,n,0,n);
+   cout << res.first << " " << res.second;
 }
 
 
---
+++ P2: Div&Conquer/MergeSelectivo.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define fore(i,a,b) for(int i = a; i< b;i++)
+
+using namespace std;
+typedef long long ll;
+
+const int INDEF = -1;
+vector<int> a; vector<int> b;
+//recurrencia dada por T(N) = 
+//Idea: 
+int solve(){
+    return 0;
+}
+
+
+int main(){
+    
+    int n,m; cin >> n >> m;
+    a.resize(n);b.resize(m);
+    fore(i,0,n) cin >> a[i];
+    fore(i,0,m) cin >> b[i];
+    //DEBEN ESTAR ORDENADOS POR REQUIERE
+    sort(a.begin(),a.end());
+    sort(b.begin(),b.end());
+
+    
+    
+
+}
+
+
